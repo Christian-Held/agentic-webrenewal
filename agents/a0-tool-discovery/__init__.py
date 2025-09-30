b924e450edb4ab8880d25b83a0bd3b2e37d63ba2
Christian Held <christianheld1981@googlemail.com>
2025-09-30 11:53:07 +0200

    Changes due to python version

---
+++ agents/a0-tool-discovery/__init__.py
@@ -0,0 +1,4 @@
+# D:\projects\helddigital\projects\agentic-webrenewal\agents\a0-tool-discovery\__init__.py
+"""
+A0 Tool-Discovery agent package.
+"""
---
+++ agents/common/__init__.py
@@ -0,0 +1,4 @@
+# D:\projects\helddigital\projects\agentic-webrenewal\agents\common\__init__.py
+"""
+Common utilities package.
+"""
